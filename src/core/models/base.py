18f24276ca380cfb3b2cf0a572780c5b847b2715
madfeelx85 <avatar.feelx@gmail.com>
2025-05-11 01:37:27 +0300

    create declarative base_ refacotr

--- src/core/models/base.py
+++ src/core/models/base.py
@@ -1,18 +1,18 @@
-from sqlalchemy.orm import (
-    DeclarativeBase,
-    Mapped,
-    mapped_column,
-    declared_attr,
-)
+from sqlalchemy import MetaData
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import declared_attr
 
+from core.config import settings
 from utils import camel_case_to_snake_case
 
 
 class Base(DeclarativeBase):
     __abstract__ = True
 
-    @declared_attr
+    metadata = MetaData(
+        naming_convention=settings.db.naming_convention,
+    )
+
+    @declared_attr.directive
     def __tablename__(cls) -> str:
         return f"{camel_case_to_snake_case(cls.__name__)}s"
-
-    id: Mapped[int] = mapped_column(primary_key=True)
