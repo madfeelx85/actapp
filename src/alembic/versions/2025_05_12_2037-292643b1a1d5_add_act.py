abf24fd53c87674b9072c5b468de417938f2bd1e
madfeelx85 <avatar.feelx@gmail.com>
2025-05-12 20:39:30 +0300

    alembic init table acts

---
+++ src/alembic/versions/2025_05_12_2037-292643b1a1d5_add_act.py
@@ -0,0 +1,53 @@
+"""add act
+
+Revision ID: 292643b1a1d5
+Revises: 1b601b7a8705
+Create Date: 2025-05-12 20:37:41.763576
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "292643b1a1d5"
+down_revision: Union[str, None] = "1b601b7a8705"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+
+    op.create_table(
+        "acts",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("name", sa.String(length=255), nullable=False),
+        sa.Column("description", sa.String(length=500), nullable=True),
+        sa.Column("build_object_id", sa.Integer(), nullable=False),
+        sa.Column("template_id", sa.Integer(), nullable=False),
+        sa.Column("data", sa.JSON(), nullable=False),
+        sa.Column("created_at", sa.DateTime(timezone=True), nullable=False),
+        sa.ForeignKeyConstraint(
+            ["build_object_id"],
+            ["build_objects.id"],
+            name=op.f("fk_acts_build_object_id_build_objects"),
+            ondelete="CASCADE",
+        ),
+        sa.ForeignKeyConstraint(
+            ["template_id"],
+            ["templates.id"],
+            name=op.f("fk_acts_template_id_templates"),
+            ondelete="SET NULL",
+        ),
+        sa.PrimaryKeyConstraint("id", name=op.f("pk_acts")),
+    )
+
+
+
+def downgrade() -> None:
+
+    op.drop_table("acts")
+
