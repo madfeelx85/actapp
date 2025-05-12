d2bb21deb546711d43dfafc2de4d5cd5fa33ff40
madfeelx85 <avatar.feelx@gmail.com>
2025-05-12 19:43:32 +0300

    add tables templates

--- src/alembic/env.py
+++ src/alembic/env.py
@@ -10,6 +10,7 @@ from alembic import context
 from core.config import settings
 from core.models import Base
 from core.models import build_object
+from core.models import template
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
---
+++ src/alembic/versions/2025_05_12_1941-1b601b7a8705_add_template.py
@@ -0,0 +1,37 @@
+"""add template
+
+Revision ID: 1b601b7a8705
+Revises: 9ae264b70711
+Create Date: 2025-05-12 19:41:07.645071
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "1b601b7a8705"
+down_revision: Union[str, None] = "9ae264b70711"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "templates",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("name", sa.String(length=255), nullable=False),
+        sa.Column("description", sa.String(length=500), nullable=True),
+        sa.Column("path", sa.String(length=500), nullable=False),
+        sa.Column("type", sa.String(length=50), nullable=False),
+        sa.PrimaryKeyConstraint("id", name=op.f("pk_templates")),
+    )
+
+
+
+def downgrade() -> None:
+    op.drop_table("templates")
+
