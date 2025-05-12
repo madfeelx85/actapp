dc7bbf4d67996b8dbadd5d2be8237e64485ac29e
madfeelx85 <avatar.feelx@gmail.com>
2025-05-12 18:19:40 +0300

    add models build_object.py

---
+++ src/core/models/build_object.py
@@ -0,0 +1,8 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String
+from core.models.base import Base
+from core.models.mixins.int_id_pk import IntIdPkMixin
+
+class BuildObject(Base, IntIdPkMixin):
+    name: Mapped[str] = mapped_column(String(255), nullable=False)
+    description: Mapped[str | None] = mapped_column(String(500), nullable=True)
