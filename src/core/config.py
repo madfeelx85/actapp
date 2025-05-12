f5a9a8bd71c278a916945c19263e0108127a6ffc
madfeelx85 <avatar.feelx@gmail.com>
2025-05-12 19:28:16 +0300

    fix config.py

--- src/core/config.py
+++ src/core/config.py
@@ -32,7 +32,7 @@ class Settings(BaseSettings):
         env_file=(".env.template", ".env"),
         case_sensitive=False,
         env_nested_delimiter="__",
-        env_prefix="API_CONFIG__",
+        env_prefix="APP_CONFIG__",
     )
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
