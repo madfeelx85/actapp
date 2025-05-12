ebb304f9ea65b3e4d3e4ccf92558ef30040b792e
madfeelx85 <avatar.feelx@gmail.com>
2025-05-13 01:28:44 +0300

    add view index.py

---
+++ src/web/view/index.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter, Request, Depends
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from sqlalchemy.ext.asyncio import AsyncSession
+from core.dependencies.db import get_db_session
+from core.models.build_object import BuildObject
+from sqlalchemy import select
+
+router = APIRouter()
+templates = Jinja2Templates(directory="templates")
+
+@router.get("/", response_class=HTMLResponse)
+async def index(request: Request, db: AsyncSession = Depends(get_db_session)):
+    result = await db.execute(select(BuildObject))
+    objects = result.scalars().all()
+    return templates.TemplateResponse("index.html", {"request": request, "objects": objects})
