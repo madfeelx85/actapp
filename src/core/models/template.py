0f564eb24e1892c795c9e8e180dae1656d60e676
madfeelx85 <avatar.feelx@gmail.com>
2025-05-12 19:39:22 +0300

    add template models

---
+++ src/core/models/template.py
@@ -0,0 +1,10 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String
+from core.models.base import Base
+from core.models.mixins.int_id_pk import IntIdPkMixin
+
+class Template(Base, IntIdPkMixin):
+    name: Mapped[str] = mapped_column(String(255), nullable=False)
+    description: Mapped[str | None] = mapped_column(String(500), nullable=True)
+    path: Mapped[str] = mapped_column(String(500), nullable=False)
+    type: Mapped[str] = mapped_column(String(50), nullable=False)  # например: "hidden_work" или "commissioning"
