987142f7dd0485ed38161a2449f53a475f2579af
madfeelx85 <avatar.feelx@gmail.com>
2025-05-12 19:40:04 +0300

    init api template.py

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,8 +1,12 @@
+from tempfile import template
+
 from fastapi import APIRouter
 
 from core.config import settings
 
 from .build_object import router as build_object_router
+from .template import router as template_router
 
 router = APIRouter(prefix=settings.api.prefix)
 router.include_router(build_object_router)
+router.include_router(template_router)
