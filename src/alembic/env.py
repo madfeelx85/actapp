5a5e4eade1fba98be5ea13595d946380fc6c6c35
madfeelx85 <avatar.feelx@gmail.com>
2025-05-12 20:37:31 +0300

    add model act in env.py

--- src/alembic/env.py
+++ src/alembic/env.py
@@ -11,6 +11,7 @@ from core.config import settings
 from core.models import Base
 from core.models import build_object
 from core.models import template
+from core.models import act
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
