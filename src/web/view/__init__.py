c42c6aa96534f14dfe087910a20ff00e990246aa
madfeelx85 <avatar.feelx@gmail.com>
2025-05-14 08:24:51 +0300

    add route for view

--- src/web/view/__init__.py
+++ src/web/view/__init__.py
@@ -4,8 +4,13 @@ from core.config import settings
 
 from .index import router as index_router
 from .object_acts import router as object_acts_router
+from .templates import router as templates_router
+from .acts import router as acts_router
 
 
 router = APIRouter()
 router.include_router(index_router)
 router.include_router(object_acts_router)
+router.include_router(templates_router)
+
+router.include_router(acts_router)
