993c258eb8d05b9c28d818317259a99e5e1f7744
madfeelx85 <avatar.feelx@gmail.com>
2025-05-14 17:30:38 +0300

    init router generate.py

--- src/web/view/__init__.py
+++ src/web/view/__init__.py
@@ -6,11 +6,12 @@ from .index import router as index_router
 from .object_acts import router as object_acts_router
 from .templates import router as templates_router
 from .acts import router as acts_router
+from .generate import router as generate_router
 
 
 router = APIRouter()
 router.include_router(index_router)
 router.include_router(object_acts_router)
 router.include_router(templates_router)
-
 router.include_router(acts_router)
+router.include_router(generate_router)
